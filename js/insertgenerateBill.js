00915a852f2ea72c7273aa331e8aac59de3c7230
Yash Talegaonkar <yash.talegaonkar37@gmail.com>
2018-09-11 17:05:06 +0530

    insert query in js
    insert query in js

---
+++ js/insertgenerateBill.js
@@ -0,0 +1,23 @@
+<script src="db.js"></script>
+
+<script>
+var value = {
+    itemName: 'Blue Jeans',
+    price: 2000,
+    quantity: 1000
+}
+
+//since Id is autoincrement column, so the value will be automatically generated.
+connection.insert({
+    into: 'Product',
+    values: [value]
+}).then(function(rowsInserted) {
+    if (rowsInserted > 0) {
+        alert('successfully added');
+    }
+}).catch(function(err) {
+    console.log(err);
+    alert(err.message);
+});
+
+</script>
